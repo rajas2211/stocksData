c8a2236258311faefeb6dc0fd2dade124a1e494a
Rajas Shah <rajas.s.shah@gmail.com>
2023-07-26 20:48:15 -0600

    Add command line functionality

--- stocksData.py
+++ stocksData.py
@@ -1,14 +1,28 @@
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
+"""This modules gathers data from Alpha Vantage API and calculates
+ daily, weekly, monthly, quarterly, half-yearly and annual time-series
+ data for stocks
+"""
+
+import os
+import sys
 import datetime
 import time
-import os
+from alpha_vantage.timeseries import TimeSeries
 import numpy as np
+import pandas as pd
 
-apiKey = 'VH10W9NDNWI32HGL'
+with open('apiKey.txt', 'r', encoding='UTF-8') as f:
+    apiKey = f.readline()
 
 
-def loadSymbols(filename):
+def load_symbols(filename):
+    """Loads a .csv file as dataFrame
+
+
+       Arguments:
+       filename -> A csv files containing all symbols. Each symbol
+       should be on a different line and contains no headers.
+    """
     # filename with csv extension
     df = pd.read_csv(filename, header=None, names=['symbols'])
     df['symbols'] = df['symbols'].str.strip()
@@ -16,89 +30,103 @@ def loadSymbols(filename):
     return df['symbols'].to_list()
 
 
-def executeSymbols(symbols):
+def execute_symbols(symbols, output_file=None):
+    """Runs script
+    """
     if isinstance(symbols, str):
         symbols = [symbols]
-    numOfSymbols = len(symbols)
+    num_symbols = len(symbols)
     dates = getDates()
-    numRequests = 0
+    num_requests = 0
     for idx, symbol in enumerate(symbols):
-        if numRequests >= 4:
-            numRequests = 0
+        if num_requests >= 4:
+            num_requests = 0
             time.sleep(60)
         # print(f"{symbol} Start")
-        print(f"Getting data for {symbol} ({idx+1}/{numOfSymbols})")
+        print(f"Getting data for {symbol} ({idx+1}/{num_symbols})")
         try:
             start_time = time.time()
-            (weekly, monthly) = getData(symbol, dates)
+            (weekly, monthly) = get_data(symbol, dates)
             end_time = time.time()
             # print(f"Duration to fetch data: {end_time - start_time} s")
             if (end_time - start_time) <= 60:
-                numRequests += 2
+                num_requests += 2
             else:
-                numRequests = 0
-            # print(f"Running API request count:{numRequests}")
+                num_requests = 0
+            # print(f"Running API request count:{num_requests}")
             df = computeData(symbol, weekly, monthly)
+            # print(df)
             # print(df)
             # print(f"Done")
         except:
-            print(f"Error occured: {symbol}")
+            print(f"Error occurred: {symbol}")
         df.insert(0, 'symbol', symbol)
-        writeMasterCSV(df)
+        write_master_csv(df, output_file)
 
 
-# def executeSymbols(symbols):
+# def execute_symbols(symbols):Runs script
 #     if isinstance(symbols, str):
 #         symbols = [symbols]
-#     numOfSymbols = len(symbols)
+#     num_symbols = len(symbols)
 #     dates = getDates()
-#     numRequests=0
+#     num_requests=0
 #     for idx, symbol in enumerate(symbols):
-#         # if numRequests >= 4:
-#         #     numRequests = 0
+#         # if num_requests >= 4:
+#         #     num_requests = 0
 #         #     time.sleep(60)
 #         # print(f"{symbol} Start")
-#         print(f"Getting data for {symbol} ({idx+1}/{numOfSymbols})")
+#         print(f"Getting data for {symbol} ({idx+1}/{num_symbols})")
 #         try:
 #             start_time = time.time()
-#             (weekly, monthly, numRequests) = getData(symbol, dates, numRequests)
+#             (weekly, monthly, num_requests) = get_data(symbol, dates,
+#                                                       num_requests)
 #             end_time = time.time()
 #             # print(f"Duration to fetch data: {end_time - start_time} s")
 #             if (end_time - start_time) > 60:
-#                 numRequests = 0
+#                 num_requests = 0
 #             # else:
-#             #     numRequests = 0
-#             # print(f"Running API request count:{numRequests}")
+#             #     num_requests = 0
+#             # print(f"Running API request count:{num_requests}")
 #             df = computeData(symbol, weekly, monthly)
 #             # print(f"Done")
 #         except:
-#             print(f"Error occured: {symbol}")
+#             print(f"Error occurred: {symbol}")
 #         try:
 #             df.insert(0, 'symbol', symbol)
 #         except:
 #             continue
-#         writeMasterCSV(df)
-
-def writeMasterCSV(df):
-    outputFile = f"masterData-{pd.to_datetime('today').strftime('%Y-%m-%d')}.csv"
-    df.to_csv(outputFile, mode='a', index=False,
-              header=not os.path.exists(outputFile))
+#         write_master_csv(df)
 
 
-def runScript(filename):
-    symbols = loadSymbols(filename)
-    executeSymbols(symbols)
+def write_master_csv(df, output_file=None):
+    """Creates output file
+    """
+    if not output_file:
+        output_file = (
+            f"masterData-{pd.to_datetime('today').strftime('%Y-%m-%d')}.csv")
+    df.to_csv(output_file, mode='a', index=False,
+              header=not os.path.exists(output_file))
+
+
+def run_script(filename, output_file=None):
+    """Runs script
+    """
+    symbols = load_symbols(filename)
+    execute_symbols(symbols, output_file)
+
     print(f"Script execution complete")
 
 
-def getData(symbol, dates, BSE=True):
+def get_data(symbol, dates, BSE=True):
+    """Get data from Alpha_Vantage
+    """
     ts = TimeSeries(key=apiKey, output_format='pandas')
     if BSE:
         symbol = symbol+'.BSE'
     try:
         # print(f"Data fetch start")
-        weekly, metaDataW = ts.get_weekly_adjusted(symbol=symbol)
-        monthly, metaDataM = ts.get_monthly_adjusted(symbol=symbol)
+        weekly, meta_data_w = ts.get_weekly_adjusted(symbol=symbol)
+        monthly, meta_data_m = ts.get_monthly_adjusted(symbol=symbol)
         # print(f"Data fetch complete")
     except Exception as e:
         print(e)
@@ -106,30 +134,30 @@ def getData(symbol, dates, BSE=True):
     return (weekly, monthly)
 
 
-# def getData(symbol, dates, numRequests= 0, BSE=True):
+# def get_data(symbol, dates, num_requests= 0, BSE=True):
 
 #     ts = TimeSeries(key=apiKey, output_format='pandas')
 #     if BSE:
 #         symbol = symbol+'.BSE'
 #     try:
 #         # print(f"Data fetch start")
-#         if numRequests >= 5 :
+#         if num_requests >= 5 :
 #             time.sleep(60)
-#             numRequests = 0
-#         weekly, metaDataW  = ts.get_weekly_adjusted(symbol=symbol)
-#         numRequests += 1
+#             num_requests = 0
+#         weekly, meta_data_w  = ts.get_weekly_adjusted(symbol=symbol)
+#         num_requests += 1
 
-#         if numRequests >= 5 :
+#         if num_requests >= 5 :
 #             time.sleep(60)
-#             numRequests = 0
-#         monthly, metaDataM = ts.get_monthly_adjusted(symbol=symbol)
-#         numRequests += 1
+#             num_requests = 0
+#         monthly, meta_data_m = ts.get_monthly_adjusted(symbol=symbol)
+#         num_requests += 1
 
 #         # print(f"Data fetch complete")
 #     except Exception as e:
 #         print(e)
 #         print(f"Error Raised")
-#     return (weekly, monthly, numRequests)
+#     return (weekly, monthly, num_requests)
 
 
 def computeData(symbol, weekly, monthly):
@@ -138,15 +166,24 @@ def computeData(symbol, weekly, monthly):
      halfBegin, halfEnd,
      yearBegin, yearEnd) = getDates()
     # print("Dates retrived")
+     quarterBegin, quarterEnd,
+     halfBegin, halfEnd,
+     yearBegin, yearEnd) = getDates()
+    # print("Dates retrived")
     weeklyData = getDictStandard(weekly, weekEnd, 'weekly')
     monthlyData = getDictStandard(monthly, monthEnd, 'monthly')
-    quarterlyData = getDictCustom(monthly, quarterBegin, quarterEnd,
-                                  'quarterly')
-    halflyData = getDictCustom(monthly, halfBegin, halfEnd, 'halfly')
-    yearlyData = getDictCustom(monthly, yearBegin, yearEnd, 'yearly')
+    quarterlyData = get_dict_custom(monthly, quarterBegin, quarterEnd,
+                                    'quarterly')
+    halflyData = get_dict_custom(monthly, halfBegin, halfEnd, 'halfly')
+    yearlyData = get_dict_custom(monthly, yearBegin, yearEnd, 'yearly')
     # print(weeklyData, monthlyData, quarterlyData, halflyData, yearlyData)
 
     rename = {
+        '1. open': 'open',
+        '2. high': 'high',
+        '3. low': 'low',
+        '4. close': 'close',
+        '5. adjusted close': 'adjclose',
         '1. open': 'open',
         '2. high': 'high',
         '3. low': 'low',
@@ -157,12 +194,15 @@ def computeData(symbol, weekly, monthly):
     df = pd.DataFrame([weeklyData, monthlyData, quarterlyData, halflyData,
                        yearlyData])
     # print(f"DF Ready")
+    # print(f"DF Ready")
     df1 = df.drop(columns=['6. volume', '7. dividend amount'])
     df2 = df1.rename(columns=rename)
+    # df2.to_csv(f"{symbol}.csv", index=False)
     # df2.to_csv(f"{symbol}.csv", index=False)
     # print(f"Export Complete")
     return df2
     # df2.to_excel(f"{symbol[:-4]}.xslx")
+
 
 
 def getDates():
@@ -171,13 +211,17 @@ def getDates():
     dayOfTheWeek = today.day_of_week
     # End of week date
     weekEnd = (today + pd.DateOffset(days=-abs(dayOfTheWeek - 4))).normalize()
+    # End of week date
+    weekEnd = (today + pd.DateOffset(days=-abs(dayOfTheWeek - 4))).normalize()
 
+    # End of Month date
     # End of Month date
     if (today.normalize() < (today + pd.offsets.BMonthEnd(n=0)).normalize()):
         monthEnd = (today + pd.offsets.MonthEnd(n=-1)).normalize()
     else:
         monthEnd = today.normalize()
 
+    # Quarter Dates
     # Quarter Dates
     if (today.normalize() < (today + pd.offsets.BQuarterEnd(n=0)).normalize()):
         quarterEnd = (today + pd.offsets.QuarterEnd(n=-1)).normalize()
@@ -186,12 +230,14 @@ def getDates():
     quarterBegin = quarterEnd + pd.offsets.QuarterBegin(n=-1, startingMonth=1)
 
     # Semiannual Dates
+    # Semiannual Dates
 
     if not today.is_year_end:
         if (today.month > 6) or (today.month == 6 and today.day == 30):
             halfEnd = (
                 (today + pd.DateOffset(months=-abs(today.month - 6)))
                 + pd.offsets.QuarterEnd(n=0)
+            ).normalize()
             ).normalize()
             halfBegin = pd.Timestamp(datetime.date(halfEnd.year, 1, 1))
         else:
@@ -202,6 +248,7 @@ def getDates():
         halfBegin = pd.Timestamp(datetime.date(halfEnd.year, 6, 1))
 
     # Annual Dates
+    # Annual Dates
 
     if (today.normalize() < (today + pd.offsets.BYearEnd(n=0)).normalize()):
         yearEnd = (today + pd.offsets.YearEnd(n=-1)).normalize()
@@ -209,17 +256,19 @@ def getDates():
         yearEnd = (today).normalize()
     yearBegin = yearEnd + pd.offsets.YearBegin(n=-1)
 
+
     return (weekEnd, monthEnd,
+            quarterBegin, quarterEnd,
             quarterBegin, quarterEnd,
             halfBegin, halfEnd,
             yearBegin, yearEnd)
 
 
-def getDictStandard(df, periodEnd, label):
-    dfData = df.loc[df.index[df.index <= periodEnd].max()]
+def getDictStandard(df, period_end, label):
+    dfData = df.loc[df.index[df.index <= period_end].max()]
     if dfData.empty:
         dictData = {'label': label,
-                    'date': periodEnd,
+                    'date': period_end,
                     '1. open': np.nan,
                     '2. high': np.nan,
                     '3. low': np.nan,
@@ -230,40 +279,46 @@ def getDictStandard(df, periodEnd, label):
                     }
     else:
         dictData = {'label': label,
-                    'date': df.index[df.index <= periodEnd].max()}
+                    'date': df.index[df.index <= period_end].max()}
         dictData = {**dictData, **dfData.to_dict()}
     return dictData
 
 
-def getDictCustom(df, periodBegin, periodEnd, label):
+def get_dict_custom(df, period_begin, period_end, label):
     periodly = df.loc[
-        df.index[(df.index <= periodEnd) & (df.index > periodBegin)]]
+        df.index[(df.index <= period_end) & (df.index > period_begin)]]
     if periodly.empty:
-        periodlyData = {'label': label,
-                        'date': periodEnd,
-                        '1. open': np.nan,
-                        '2. high': np.nan,
-                        '3. low': np.nan,
-                        '4. close': np.nan,
-                        '5. adjusted close': np.nan,
-                        '6. volume': np.nan,
-                        '7. dividend amount': np.nan,
-                        }
+        periodly_data = {'label': label,
+                         'date': period_end,
+                         '1. open': np.nan,
+                         '2. high': np.nan,
+                         '3. low': np.nan,
+                         '4. close': np.nan,
+                         '5. adjusted close': np.nan,
+                         '6. volume': np.nan,
+                         '7. dividend amount': np.nan,
+                         }
     else:
-        periodlyData = {'label': label,
-                        'date': periodly.index[0],
-                        '1. open': periodly['1. open'].iloc[-1],
-                        '2. high': periodly['2. high'].max(),
-                        '3. low': periodly['3. low'].min(),
-                        '4. close': periodly['4. close'].iloc[0],
-                        '5. adjusted close': periodly['5. adjusted close'].iloc[0],
-                        '6. volume': periodly['6. volume'].sum(),
-                        '7. dividend amount': periodly['7. dividend amount'].sum(),
-                        }
-    return periodlyData
+        periodly_data = {'label': label,
+                         'date': periodly.index[0],
+                         '1. open': periodly['1. open'].iloc[-1],
+                         '2. high': periodly['2. high'].max(),
+                         '3. low': periodly['3. low'].min(),
+                         '4. close': periodly['4. close'].iloc[0],
+                         '5. adjusted close': periodly['5. adjusted close'].iloc[0],
+                         '6. volume': periodly['6. volume'].sum(),
+                         '7. dividend amount': periodly['7. dividend amount'].sum(),
+                         }
+    return periodly_data
 
 
-# executeSymbols('NSLNISP')
-# ts = TimeSeries(key=apiKey, output_format='pandas')
-# weekly = ts.get_weekly_adjusted(symbol='NSLNISP.BSE')
-# print(weekly)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        DATA_FILE = sys.argv[1]
+        OUTPUT_FILE = None
+        if len(sys.argv) > 2:
+            OUTPUT_FILE = sys.argv[2]
+    else:
+        DATA_FILE = "symbolsT.csv"
+        OUTPUT_FILE = None
+    run_script(DATA_FILE, OUTPUT_FILE)
